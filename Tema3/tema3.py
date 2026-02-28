ce98514023cfffeea519759a5d5c8d7f11040ecd
Georgiana Matcovici <georgianamatcovici@gmail.com>
2026-02-28 21:47:00 +0200

    tema 3

--- Tema3/tema3.py
+++ Tema3/tema3.py
@@ -1,4 +1,4 @@
-from cmath import sqrt
+import math
 import copy
 import random
 import numpy as np
@@ -23,22 +23,11 @@ if(x==1):
 else:
      n = int(input("n:"))
      eps=float(input("eps:"))
-     B = [[0]*n for i in range(0, n)]
+     
+     A = [[0]*n for i in range(0, n)]
      for i in range(0, n):
          for j in range(0, n):
-             B[i][j]=random.random()
-
-     A=[[0]*n for i in range(0, n)]
-
-     for i in range(0, n):
-         for j in range(0, n):
-              s = 0
-              for k in range(n):
-                    s += B[i][k] * B[j][k]
-              A[i][j] = s
-
-     for i in range(n):
-        A[i][i] += n
+             A[i][j]=random.uniform(-100, 100)
      
      s=[0]*n
 
@@ -52,6 +41,7 @@ print("eps =", eps)
 # print("A =\n", A)
 # print("s=", s)
 
+#Cerinta 1 - calcul b
 b = []
 for i in range(n):
     suma_rand = 0
@@ -59,9 +49,13 @@ for i in range(n):
         suma_rand += A[i][j] * s[j]
     b.append(suma_rand)
 
-# print("b =", b)
+if(x==1): print("b =", b)
+
+
 b_copy=copy.deepcopy(b)
 
+
+#Cerinta 2: descompunere QR folosind Householder
 Q_T=[[0]*n for i in range(0, n)]
 for i in range(0, n):
     Q_T[i][i]=1
@@ -75,7 +69,7 @@ for r in range(0, n-1):
     # print(sigma)
     if(sigma<=eps):
         break
-    k=sqrt(sigma)
+    k=math.sqrt(sigma)
     if(A[r][r].real>0):
         k=-k
     beta=sigma-k*A[r][r]
@@ -114,19 +108,26 @@ for r in range(0, n-1):
 
         for i in range(r, n):
             Q_T[i][j]=Q_T[i][j]-gama*u[i]
-# print(A)
-# print(Q_T)
+if(x==1):
+    print("R",A)
+    print("Q transpus", Q_T)
 
 
-# print(A_copy)
+# R- superior triunghiulara stocata in A, Q_T transpusa lui Q, b=Q_T*b_init
+
+
+# Cerinta 3: rezolvare sistem 
+
 Q1, R1 = np.linalg.qr(A_copy)
-# print("Q1", Q1)
-# print("R1", R1)
+
+if(x==1):
+    print("Q1", Q1)
+    print("R1", R1)
 
 
 b_sistem=[0]*n
 b_sistemQR=[0]*n
-# print("Q_T", Q_T)
+
 
 for i in range(0, n):
     sum=0 
@@ -137,10 +138,12 @@ for i in range(0, n):
     b_sistem[i]=sum
     b_sistemQR[i]=sum1
 
-#Ax=b
 
-# print("b", b_sistem)
-# print("bQR", b_sistemQR)
+if(x==1):
+    print("b", b_sistem)
+    print("bQR", b_sistemQR)
+
+
 x_householder=[0]*n
 x_QR=[0]*n
 for i in range(n-1, -1, -1):
@@ -152,8 +155,9 @@ for i in range(n-1, -1, -1):
     x_householder[i]=(b_sistem[i]-sum)/A[i][i]
     x_QR[i]=(b_sistemQR[i]-sum1)/R1[i][i]
 
-# print("x", x_householder)
-# print("x", x_QR)
+if(x==1):
+    print("x", x_householder)
+    print("x", x_QR)
 
 norma=0
 for i in range(0, n):
@@ -161,6 +165,8 @@ for i in range(0, n):
 
 print("Norma solutiei x", norma)
 
+
+#CErinta 4: erori
 err1=0
 err2=0
 dif1=[0]*n
@@ -170,17 +176,17 @@ for i in range(0, n):
     res1=0
     res2=0
     for j in range(0, n):
-          res1=res1+A[i][j]*x_householder[j] 
-          res2=res2+A[i][j]*x_QR[j] 
+          res1=res1+A_copy[i][j]*x_householder[j] 
+          res2=res2+A_copy[i][j]*x_QR[j] 
 
-    dif1[i]=res1-b[i]
-    dif2[i]=res2-b[i]
+    dif1[i]=res1-b_copy[i]
+    dif2[i]=res2-b_copy[i]
 
     err1=err1+dif1[i]**2
     err2=err2+dif2[i]**2
 
-print("Eroare1: ", sqrt(err1))
-print("Eroare2: ", sqrt(err2))
+print("Eroare1: ", math.sqrt(err1))
+print("Eroare2: ", math.sqrt(err2))
 
 
 err3=0
@@ -196,21 +202,23 @@ for i in range(0, n):
     norma3+=(x_QR[i]-s[i])**2
   
 
-err3=sqrt(norma1)/sqrt(norma2)
-err4=sqrt(norma3)/sqrt(norma2)
+err3=math.sqrt(norma1)/math.sqrt(norma2)
+err4=math.sqrt(norma3)/math.sqrt(norma2)
 
-print("Eroare3: ", sqrt(err3))
-print("Eroare4: ", sqrt(err4))
+print("Eroare3: ", err3)
+print("Eroare4: ", err4)
 
+
+#Cerinta 5: inversa
 
 det=1
 for i in range(0, n):
-    if(A[i][i].real<eps):
+    if(abs(A[i][i])<eps):
          print("Nu putem calcula inversa")
          break
     det=det*A[i][i]
 
-print(det)
+print("Determinant:", det)
 
 
 Inv_A=[[0]*n for  i in range(0, n)]
@@ -228,15 +236,17 @@ for j in range(n):
             sum=sum+A[k][l]*x_star[l]
         x_star[k]=(coloana_j[k]-sum)/A[k][k]
         Inv_A[k][j]=x_star[k]
-    
-# print(Inv_A)
+
+if(x==1): 
+ print("Inversa", Inv_A)
 
 Inv_A_bibl = np.linalg.inv(A_copy)
-# print(Inv_A_bibl)
+if(x==1):
+  print("Inversa Librarie", Inv_A_bibl)
 
 norma_inversa=0
 for i  in range(0, n):
     for j in range(0, n):
         norma_inversa+=(Inv_A[i][j]-Inv_A_bibl[i][j])**2
-print("Norma intre inverse matrici", sqrt(norma_inversa))
+print("Norma intre inverse matrici", math.sqrt(norma_inversa))
 
